826a34a14786e00783d882e3057e6d46ba8cb28d
msyesyan <msyesyan@gmail.com>
2021-12-30 11:55:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-zorro-antd-ivy-uda7th
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-zorro-antd-ivy-uda7th)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "my-app",
+  template: `
+   <form [formGroup]="formGroup">
+      <button nz-button nzType="primary" (click)="toggle()">
+        Enable
+      </button>
+
+      <textarea nz-input [disabled]="!enable.value"></textarea>
+    </form>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  formGroup = new FormGroup({
+    enable: new FormControl(false),
+    text: new FormControl(null)
+  })
+
+  get enable(): FormControl {
+    return this.formGroup.get('enable') as FormControl;
+  }
+
+  get disabled() {
+    return !this.formGroup.get('enable').value
+  }
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  toggle() {
+    this.cdr.markForCheck();
+    const disabled = this.disabled;
+
+    if (this.enable.value) {
+      this.formGroup.patchValue({enable: false});
+    } else {
+      this.formGroup.patchValue({enable: true});
+    }
+  }
+}
